2cc681dbf414ce4b12cf2887be6de8c5972c2a6a
George Melidis <melidis@gmail.com>
2016-10-03 19:46:34 +0300

    modify task bor seperate image and css

--- gulp/tasks/sprite.smith.js
+++ gulp/tasks/sprite.smith.js
@@ -1,11 +1,13 @@
-+'use strict';
-+
-  +module.exports = function() {
-  +    $.gulp.task('sprite:smith', function () {
-    +        var spriteData = $.gulp.src('./source/images/icons/*.{png,gif,jpg,jpeg}').pipe($.gp.spritesmith({
-      +            imgName: 'sprite.png',
-      +            cssName: 'sprite.css'
-    +        }));
-    +        return spriteData.pipe($.gulp.dest($.config.root + '/assets/img'));
-    +    });
-  +};
+'use strict';
+
+module.exports = function() {
+  $.gulp.task('sprite:smith', function () {
+    var spriteData = $.gulp.src('./source/images/icons/*.{png,gif,jpg,jpeg}') // source path of the sprite images
+      .pipe($.gp.spritesmith({
+      imgName: 'sprite.png',
+      cssName: 'sprite.css'
+    }));
+    return spriteData.img.pipe($.gulp.dest($.config.root + '/assets/img')),
+      spriteData.css.pipe($.gulp.dest($.config.root + '/assets/css'))     ;
+    });
+  };
